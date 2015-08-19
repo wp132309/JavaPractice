d9b2bf40baacaa0863b4b2559050333185c7f192
wp132309 <qwer7qwe6@naver.com>
2015-08-19 12:33:34 +0900

    첫번째 실습과제 제출

--- JavaPractice/src/org/dimigo/inheritance/FigureTest.java
+++ JavaPractice/src/org/dimigo/inheritance/FigureTest.java
@@ -26,8 +26,8 @@ public class FigureTest {
 		Rectangle r = new Rectangle(20, 20, 5, 8);
 		
 		System.out.println("원의 넓이 : " + String.format("%.1f", c.calcArea())); 
-		System.out.println("삼각형 넓이 : " + t.calcArea()); 
-		System.out.println("사각형 넓이 : " + r.calcArea()); 
+		System.out.println("삼각형 넓이 : " + t.calcArea());
+		System.out.println("사각형 넓이 : " + r.calcArea());
 		
 		System.out.println(); 
 		
